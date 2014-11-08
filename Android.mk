86dcabb6a8e18549b3ed7af65b5c1a6b2b47f632
timduru <timduru@gmail.com>
2014-11-08 11:45:03 +0100

    build fix

--- Android.mk
+++ Android.mk
@@ -4,7 +4,8 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= thtt.c
 LOCAL_MODULE := thtt
 LOCAL_MODULE_TAGS := optional
-LOCAL_STATIC_LIBRARIES := libcutils libc
+LOCAL_STATIC_LIBRARIES := libcutils
+#LOCAL_FORCE_STATIC_EXECUTABLE := true
 include $(BUILD_EXECUTABLE)
 
 
